6d9ff2555739ae6bcbaa269ab0dbc74b31deb565
sathish4mailing <sathish4mailing@yahoo.co.in>
2019-12-21 21:28:20 +0530

    New Feature Release

--- WalletAPIEdureka/src/com/edureka/wallet/WalletApiApplication.java
+++ WalletAPIEdureka/src/com/edureka/wallet/WalletApiApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WalletApiApplication {
 
+	public WalletApiApplication() {
+		System.out.println(getClass().getName());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WalletApiApplication.class, args);
 	}
 }
+
+
